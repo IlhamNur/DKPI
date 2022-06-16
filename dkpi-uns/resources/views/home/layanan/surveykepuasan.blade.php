46a9830e3d4c3fa34af9d617f92df5a08dceeb2d
Ilham Nur <romdhoninuril@gmail.com>
2022-06-16 14:58:13 +0700

    first commit

---
+++ dkpi-uns/resources/views/home/layanan/surveykepuasan.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.main')
+@section('container')
+
+<!-- ======= Survey Kepuasan Section ======= -->
+<br>
+<br>
+<section id="data-pdln" class="services section-bg">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <h2>Survey Kepuasan</h2>
+          	<p>Kami menyampaikan terima kasih dan penghargaan atas kesediaan Bapak/Ibu menjadi mitra di Universitas Sebelas Maret (UNS). Dalam rangka upaya peningkatan kualitas pelayanan, kami mohon kesediaan Bapak/Ibu untuk mengisi kuesioner ini berdasarkan pelayanan yang telah kami berikan selama instansi Bapak/Ibu bekerja sama dengan Universitas Sebelas Maret (UNS). Hasil isian kuesioner ini akan sangat bermanfaat bagi pengembangan pelayanan kami selanjutnya. Terima kasih.</p>
+        </div>
+
+      	<center>
+         	<div class="d-grid gap-5 d-md-block">
+	            <a class="btn btn-primary" type="button" href="https://forms.gle/fz9wePCPBnxvzcvK6"><i>&nbsp;Isi Survey</i></a>
+            </div>
+         </center>
+
+     </div>
+</section> <!-- End Section -->
+@endsection
+
---
+++ dkpi-uns/storage/framework/views/2d27f6cba3033c26e343ce14f5367183406afaee.php
@@ -0,0 +1,26 @@
+
+<?php $__env->startSection('container'); ?>
+
+<!-- ======= Survey Kepuasan Section ======= -->
+<br>
+<br>
+<section id="data-pdln" class="services section-bg">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <h2>Survey Kepuasan</h2>
+          	<p>Kami menyampaikan terima kasih dan penghargaan atas kesediaan Bapak/Ibu menjadi mitra di Universitas Sebelas Maret (UNS). Dalam rangka upaya peningkatan kualitas pelayanan, kami mohon kesediaan Bapak/Ibu untuk mengisi kuesioner ini berdasarkan pelayanan yang telah kami berikan selama instansi Bapak/Ibu bekerja sama dengan Universitas Sebelas Maret (UNS). Hasil isian kuesioner ini akan sangat bermanfaat bagi pengembangan pelayanan kami selanjutnya. Terima kasih.</p>
+        </div>
+
+      	<center>
+         	<div class="d-grid gap-5 d-md-block">
+	            <a class="btn btn-primary" type="button" href="https://forms.gle/fz9wePCPBnxvzcvK6"><i>&nbsp;Isi Survey</i></a>
+            </div>
+         </center>
+
+     </div>
+</section> <!-- End Section -->
+<?php $__env->stopSection(); ?>
+
+
+<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/dkpiuns/dkpi-uns/resources/views/home/layanan/surveykepuasan.blade.php ENDPATH**/ ?>
